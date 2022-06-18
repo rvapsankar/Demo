8e249134ea5dd646cec668269859246529634ece
sankarasulike@gmail.com <sankarasulike@gmail.com>
2022-06-18 17:59:06 +0530

    delete sop statements

--- GitEee/src/Testcase1.java
+++ GitEee/src/Testcase1.java
@@ -2,15 +2,13 @@
 public class Testcase1 {
 
 	public static void main(String[] args) {
-	System.out.println("Testcage");
+	    System.out.println("Testcage");
         System.out.println("Testcage");
          System.out.println("Testcage");
-	System.out.println("Testcage");
-	System.out.println("Testcage");
-        System.out.println("Testcage");
-         System.out.println("Testcage");
-	System.out.println("Testcage");
-	System.out.println("Testcage");
+	     System.out.println("Testcage");
+	
+	
+	
 	
        
 	}
