ed7f5613b849793e580a7b6e9d45c2b5ee3c42cc
rvapsankar <59088429+rvapsankar@users.noreply.github.com>
2022-06-18 18:25:18 +0530

    Update Testcase1.java
    delete one more element

--- GitEee/src/Testcase1.java
+++ GitEee/src/Testcase1.java
@@ -2,10 +2,9 @@
 public class Testcase1 {
 
 	public static void main(String[] args) {
-	    System.out.println("Testcage");
-        System.out.println("Testcage");
-         System.out.println("Testcage");
-	     System.out.println("Testcage");
+	    System.out.println("Testcase1");
+       
+        
 	
 	
 	
