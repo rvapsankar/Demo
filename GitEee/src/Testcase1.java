ac2c583dce34a32b0afab16e3fe1b93e2c8c5a47
sankarasulike@gmail.com <sankarasulike@gmail.com>
2022-06-18 21:40:03 +0530

    commentone sop

--- GitEee/src/Testcase1.java
+++ GitEee/src/Testcase1.java
@@ -2,7 +2,7 @@
 public class Testcase1 {
 
 	public static void main(String[] args) {
-	    System.out.println("Testcase1");
+	   // System.out.println("Testcase1");
 	    System.out.println("Testcase12");
 	    System.out.println("Testcase123");
 		  System.out.println("Testcase1234");
